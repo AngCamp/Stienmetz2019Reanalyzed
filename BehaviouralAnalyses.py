d351f9dc940d8462435d8e82e962780c38753d8b
Angus Campbell <angus.campbell@ubc.ca>
2022-04-06 13:54:35 -0700

    added some readings to BehaviouralAnalysis.py

--- BehaviouralAnalyses.py
+++ BehaviouralAnalyses.py
@@ -15,6 +15,32 @@ Akrami, A., Kopec, C. D., Diamond, M. E., & Brody, C. D. (2018).
 
  Espescially relevant is extended data 6 where they describe other models to the one they found was the best fit.
  https://www.nature.com/articles/nature25510/figures/10
+"""
+
+#CODE GOES HERE
+
+
+
+
+
+
+"""
+Furhter Readings:
+Other Effects to look into:
+PPC as a history buffer, is this accomplished via hippocampus or more locally driven?
+
+ Bitzidou, M., Bale, M. R., & Maravall, M. (2018). Cortical Lifelogging: the posterior parietal cortex as sensory history buffer. 
+Neuron, 98(2), 249-252.
+
+Jarzebowski, P. (2022). Encoding and recall of memory for reward location in the mouse hippocampus
+ (Doctoral dissertation, University of Cambridge).
+https://www.repository.cam.ac.uk/handle/1810/334631
+-pdf downloaded in Seteinmetzlab on your pc as PhDThesis-Jarzebowski-2022.pdf
+
+Worth reading as well:
+Zabeh, E., Foley, N. C., Jacobs, J., & Gottlieb, J. P. (2022). 
+Traveling waves in the monkey frontoparietal network predict recent reward memory. bioRxiv.
+https://www.biorxiv.org/content/10.1101/2022.02.03.478583v1.abstract
 
 
 """
